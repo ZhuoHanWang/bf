0d2c0b72861939664a00c2d13590bf8cb22818b9
卓涵 王 <2642653418@qq.com>
2024-06-01 12:17:27 +0800

    这一次注意到了，组件库还有个uno组件库

--- src/components/left/rowDemo.vue
+++ src/components/left/rowDemo.vue
@@ -1,23 +1,19 @@
 <template>
-    <div>
-        <!-- 这里是左侧页面的一行，做成插槽方便控制，左侧为图标，中间为提示文本，右侧为选择参数 -->
-
-
-        <!-- 这里是显示图片的插槽 -->
-        <div class="image-display">
-            <slot name="image"></slot>
-        </div>
-        <!-- 这是中间那条线 -->
-        <div small-bg>
-            <dv-decoration8 :reverse="true" style="width:300px;height:50px;" />
-        </div>
-        <!-- 默认的颜色是蓝色 -->
-        <dv-button @click="console.log('click')" border="Border3">
-            <!-- 这里放按钮的名字 -->
-            <slot name="buttonName"></slot>
-        </dv-button>
+  <div class="flex items-center">
+    <!-- 左侧为图标 -->
+    <div class="mr-2">
+      <slot name="image"></slot>
     </div>
+    <div small-bg>
+      <dv-decoration8 :reverse="true" style="width:300px;height:50px;" />
+    </div>
+    <!-- 右侧为选择参数按钮 -->
+    <div class="ml-2">
+      <dv-button @click="console.log('插槽组件的按钮被按下')" border="Border3">
+        <slot name="buttonName"></slot>
+      </dv-button>
+    </div>
+  </div>
 </template>
-
 <script setup lang="ts">
 </script>
--- src/components/left/up/run-param.vue
+++ src/components/left/up/run-param.vue
@@ -1,22 +1,23 @@
 <template>
   <div>
-    <div class="flex justify-center items-center pt-2">
-      <dv-decoration-11 style="width:100px;height:20px;" >
-          <div color-green font-600 bg="~ dark/0" >
-            联邦学习运行前的准备参数设置
-          </div>
+    <div class="flex pt-2">
+      <!-- 将 dv-decoration-11 居左显示 -->
+      <dv-decoration-11 style="width:400px;height:60px;" class="flex-shrink-0">
+        <div class="text-white text-xs font-thin bg-black bg-opacity-50">
+          联邦学习运行前的参数准备设置
+        </div>
       </dv-decoration-11>
-      <rowDemo>
-        <!-- 这里放的是logo -->
-        <template v-slot:image>
-          <img src="C:\Users\wangzhuohan\Desktop\孙哲实验室\bf\src\assets\imgs\模型.png" alt="Image Alt Text">
-        </template>
-        <!-- 这里放的是按钮的名字 -->
-        <template v-slot:buttonName>
-          CNN
-        </template>
-      </rowDemo>
     </div>
+    <rowDemo>
+      <!-- 这里放的是logo -->
+      <template v-slot:image>
+        <img src="C:\Users\wangzhuohan\Desktop\孙哲实验室\bf\src\assets\imgs\模型.png" alt="Image Alt Text">
+      </template>
+      <!-- 这里放的是按钮的名字 -->
+      <template v-slot:buttonName>
+        CNN
+      </template>
+    </rowDemo>
   </div>
 </template>
 
